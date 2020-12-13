e1680953a22bf73f1727aa40176d33b3d317a843
ilhami <ilhami18@mhs.unsyiah.ac.id>
2020-12-14 00:09:29 +0700

    Splash Screen dan halaman utama sprint 1

---
+++ app/src/main/java/com/example/speechtotext/ToolbarActivity.java
@@ -0,0 +1,39 @@
+package com.example.speechtotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.ex_menu,menu);
+        return true;
+
+
+    }
+
+
+    }
